73788c8d26e92d4eefc2105f61326f87c766902f
inrain9953 <inrainconstruction46@gmail.com>
2026-09-23 15:36:39 +0530

    site.webmanifest and favicon changes

--- components/Schema/StaticMetatag.js
+++ components/Schema/StaticMetatag.js
@@ -41,13 +41,7 @@ const StaticMetatag = ({ data }) => {
         rel='icon'
         type='image/png'
         sizes='32x32'
-        href='/favicon-32x32.png'
-      />
-      <link
-        rel='icon'
-        type='image/png'
-        sizes='16x16'
-        href='/favicon-16x16.png'
+        href='/favicon-96x96.png'
       />
       <link
         rel='icon'
